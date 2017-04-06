483a881b60a772dd8d44dee60dad29a5ca99671e
Richard Robinson <r.jason.robinson12@gmail.com>
2017-04-05 18:51:35 -0700

    Generalized input data so that separate sources can be used.

--- hotspot_locator.py
+++ hotspot_locator.py
@@ -11,7 +11,8 @@ SEARCH_RADIUS = 500  # feet
 CITIES        = ["Cincinnati", "Dayton", "Akron", "Columbus", "Cleveland"]
 OUTPUT_DIR    = 'output\\{}ft Search Radius -- {}'.format(str(SEARCH_RADIUS),
                                                           dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
-INPUT_DIR     = 'data'
+INPUT_DIR_1     = 'data\\points'
+INPUT_DIR_2     = 'data\\places'
 
 # cityCoords = [
 #               (39.102674, -84.512615),  # Cincinnati
@@ -26,7 +27,7 @@ def get_euclids(args):
     city, points, taskPoints = args
 
     # Iterate through points
-    neighbors = pd.DataFrame(columns=['cnt', 'distFeet'], index=taskPoints.index)
+    neighbors = pd.DataFrame(columns=['Latitude', 'Longitude', 'cnt', 'matches'], index=taskPoints.index)
     i = 0
     for k, p in taskPoints.iterrows():
         i += 1
@@ -46,7 +47,7 @@ def get_euclids(args):
             dist = points.apply(euclid, axis=1)
         msk = ((dist < SEARCH_RADIUS) & (dist > 0))
         matches = set(dist.index[msk])
-        neighbors.loc[(p.Latitude, p.Longitude), ['cnt', 'distFeet']] = (len(matches), matches)
+        neighbors.loc[k] = (p.Latitude, p.Longitude, len(matches), matches)
 
     return neighbors.loc[neighbors.cnt > 0]
 
@@ -56,8 +57,13 @@ if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    # Debug without multiprocessing
-    # get_euclids(("Cincinnati", pd.DataFrame.from_csv("2016 Recoveries {}.csv".format("Cincinnati")).head(150)))
+    # # Debug without multiprocessing
+    # primaryPtsFile = "{}\\{}.csv".format(INPUT_DIR_1, "Dayton")
+    # secondaryPtsFile = "{}\\{}.csv".format(INPUT_DIR_2, "Dayton")
+    # df = get_euclids(("Dayton",
+    #                  pd.DataFrame.from_csv(primaryPtsFile),
+    #                  pd.DataFrame.from_csv(secondaryPtsFile).head(150)))
+    # df.to_csv('{}\\{}.csv'.format(OUTPUT_DIR, "Dayton"))
 
     # Break each city into groups and process simultaneously
     ts2 = dt.datetime.now()
@@ -65,19 +71,28 @@ if __name__ == '__main__':
         ts1 = dt.datetime.now()
         hotSpots = None
         processCnt = 6
-        inFileName = "{}\\{}.csv".format(INPUT_DIR, cityName)
+        primaryPtsFile = "{}\\{}.csv".format(INPUT_DIR_1, cityName)
+        secondaryPtsFile = "{}\\{}.csv".format(INPUT_DIR_2, cityName)
+        # Load primary (large) points dataset, will check if these are near secondary
         try:
-            repoPoints = pd.DataFrame.from_csv(inFileName)[['Longitude', 'Latitude']].head(100)
+            primaryPts = pd.DataFrame.from_csv(primaryPtsFile)[['Longitude', 'Latitude']]
         except Exception as e:
-            print("Failed to load {}, ".format(inFileName, e))
-            break
+            print("Failed to load {}, ".format(primaryPtsFile, e))
+            continue
 
-        repoPoints.index = [(v.Latitude, v.Longitude) for k, v in repoPoints.iterrows()]
-        taskGroups = pd.cut(range(len(repoPoints)), processCnt, labels=range(processCnt))
+        # Load secondary (smaller) points dataset, will check if primaryPts are near these
+        try:
+            secondaryPts = pd.DataFrame.from_csv(secondaryPtsFile)[['Longitude', 'Latitude']]
+        except Exception as e:
+            print("Failed to load {}, e={}".format(secondaryPtsFile, e))
+            continue
+
+        primaryPts.index = [(v.Latitude, v.Longitude) for k, v in primaryPts.iterrows()]
+        taskGroups = pd.cut(range(len(secondaryPts)), processCnt, labels=range(processCnt))
 
         with mp.Pool(processCnt) as pool:
             resList = pool.map(get_euclids,
-                               ((cityName, repoPoints, repoPoints.loc[taskGroups == b]) for b in taskGroups.categories))
+                               ((cityName, primaryPts, secondaryPts.loc[taskGroups == b]) for b in taskGroups.categories))
 
         hotSpots = pd.concat(resList)
         hotSpots.to_csv('{}\\{}.csv'.format(OUTPUT_DIR, cityName))
